a812bf6df98aeaf56cae3ce12ba777471bd2092f
lncd <lncd@eegtask>
2025-10-31 13:47:57 -0400

    lpt_test.py: run on linux

--- lpt_test.py
+++ lpt_test.py
@@ -3,19 +3,30 @@
 Test TTL pulse timing.
 Recording timing can be tested with lpt_timing.py
 """
+from subprocess import run
 from psychopy import core, parallel
-PORT = 53264  # /dev/parport0
-SENDTTL = False
 
+# do we actually use the parallel port? or just testing?
+ports = {
+        'eegtask': "/dev/parport0", # 2025-10-31 - linux eeg
+        "xxx": 53264 # 2025-10 - win11
+        }
+#host = os.environ.get("HOSTNAME","")
+host = run("hostname", capture_output=True).stdout.decode().strip()
+port = ports.get(host, None)
 pp = None
-if SENDTTL:
-    pp = parallel.ParallelPort(address=PORT)
+if port:
+    pp = parallel.ParallelPort(address=port)
+    print(f"# Using port {port} on  host '{host}'")
+else:
+    print(f"# Warning: host '{host}' does not have an assocated port. printing only")
+
 
 def ttl(val, wait=0.100):
     """Send trigger value to parallel port then clear pins.
     @param val   value to send between 1 and 255
     @param wait  time in seconds to wait before clearing pins (ttl=0)"""
-    if not SENDTTL:
+    if not pp:
         #print(f"{core.getTime():02.04f}\t{val}, wait {wait}")
         print(f"# TTL {val} not sent, waiting {wait}")
         core.wait(wait)
